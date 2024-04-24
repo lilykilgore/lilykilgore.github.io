6090e1b3470ba77ea9c4739819f61b123cc68b76
lilykilgore <144718722+lilykilgore@users.noreply.github.com>
2024-04-24 18:36:28 +0000

    task 1

---
+++ scripts/main.js
@@ -0,0 +1,13 @@
+console.log("text");
+
+console.log("Task 1");
+for(let i = 1; i <=10; i++) {
+    console.log(i*i);
+}
+console.log("Task 2")
+
+console.log("Task 3")
+
+console.log("Task 4")
+
+console.log("Task 5")
