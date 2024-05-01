81b5a436124b740e20dad4c7fb89993e762bec9a
lilykilgore <144718722+lilykilgore@users.noreply.github.com>
2024-05-01 18:14:10 +0000

    task 4

--- scripts/main.js
+++ scripts/main.js
@@ -10,12 +10,22 @@ for (let i = 5; i >= 1; i--) {
 }
 console.log("Blastoff");
 console.log("Task 3")
-for (let i = 0; i <= 50; i+= 2) {
-    console.log(i)
+for (let i = 0; i <= 50; i = i + 2) {
+    console.log(i);
 }
 console.log("Task 4")
+let sum = 0;
 for (let i = 1; i <= 100; i++){
-    console.log(i)
+    sum = sum + i;
 }
+console.log(sum);
 
 console.log("Task 5")
+for (let i = 1; i <= 10; i++){
+    let factorial = 1
+for (let j = 1; j <=i; j++){
+    factorial *= 1;
+
+}
+    console.log(factorial);
+}
