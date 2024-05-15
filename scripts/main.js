8d33a2e56d572346ad319e42a243df3b649cdf38
lilykilgore <144718722+lilykilgore@users.noreply.github.com>
2024-05-15 17:36:22 +0000

    task 8 9 10

--- scripts/main.js
+++ scripts/main.js
@@ -27,3 +27,38 @@ for (let i = 1; i <= 10; i++) {
     console.log(numbersFactorial);
 
 }
+console.log("Task 6")
+let arr = [3, 1, 4, 1, 5];
+for (let i = 0; i < arr.length; i++) {
+    let element = arr[i];
+    console.log(element);
+}
+console.log("Task 7")
+for (let i = 0; i < arr.length; i++){
+    let sequence = arr.at(-1*i-1)
+    console.log(sequence)
+}
+console.log("Task 8")
+let cubeNumbers = []
+for (let i = 1; i <=10; i++){
+    cubeNumbers.push(i ** 3);
+}
+console.log(cubeNumbers);
+
+console.log("Task 9")
+let fibonacciArray = [];
+let a = 0, b = 1
+for (let i = 0; i < 10; i++){
+    fibonacciArray.push(a);
+    let nextNumber = a + b;
+    a = b;
+    b = nextNumber;
+}
+console.log(fibonacciArray)
+
+console.log("Task 10")
+let reverseArray = []
+for (let i = arr.length - 1; i >= 0; i--) {
+    reverseArray.push(arr[i]);
+}
+console.log(reverseArray)
